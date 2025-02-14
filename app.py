853d768b081c50684dcb5bf55b31c1bf87ab92c5
pingypw <pingpwqq@gmail.com>
2025-02-14 14:40:52 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ st.title("My first ML App (Study on Imbalanced Data Classification sick_euthyroi
 
 # Input fields
 features = []
-
+title = '77,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,1.2,0,1,0.6,0,1,71,0,1,0.68,0,1,104,1,0'
 title = st.text_input("Text Input, Enter 42 features with comma separated")
 st.write("example; feature_1,feature_2, .... , feature_n")
 features = [float(i) for i in title.split(',')]
 
 # Prediction
-if st.button("Predict"):
+if st.button("Predict") and title != 'default':
     prediction = model.predict([np.array(features)])
     st.write(f"Predicted Class: {prediction[0]}")
